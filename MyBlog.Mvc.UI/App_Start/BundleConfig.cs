cbc9cc2345de48b46945140533f5e22daedae048
Muhammed Emre Şimşek <m.emre.siimseek@gmail.com>
2020-07-06 10:34:04 +0300

    Refactoring bundle config and UI design

--- MyBlog.Mvc.UI/App_Start/BundleConfig.cs
+++ MyBlog.Mvc.UI/App_Start/BundleConfig.cs
@@ -10,8 +10,12 @@ namespace MyBlog.Mvc.UI.App_Start
     {
         public static void RegisterRoutes(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/css/all").Include("~/Content/bootstrap.min.css"));
+            bundles.Add(new StyleBundle("~/css/all").Include("~/Content/css/main-layout.css", "~/Content/bootstrap.min.css", "~/Content/css/font-awesome.min.css"));
             bundles.Add(new ScriptBundle("~/js/all").Include("~/Scripts/bootstrap.min.js", "~/Scripts/jquery-3.5.1.min.js"));
+
+            bundles.Add(new StyleBundle("~/temp/css").Include("~/assets/css/*.css"));
+            bundles.Add(new ScriptBundle("~/temp/js").Include("~/assets/js/*.js"));
+
 
         }
     }
